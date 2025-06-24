d9bb3be27a5c67e77d1ece09f154510b4ad6b484
nickbestlovesfoxes <nickbestlovesfoxs@proton.me>
2025-06-23 21:16:37 -0500

    shoping cart

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,11 +1,22 @@
+import { useCart } from './CartContext';
+
 interface AddToCartButtonProps {
   className?: string;
+  shirt: {
+    id: number;
+    name: string;
+    price: string;
+    image: string;
+  };
 }
 
-export const AddToCartButton: React.FC<AddToCartButtonProps> = ({ className }) => {
+export const AddToCartButton: React.FC<AddToCartButtonProps> = ({ className, shirt }) => {
+  const { addToCart } = useCart();
+
   return (
     <button 
-      className={` add-to-cart-button w-full py-3 px-6  ${className}`}
+      className={`add-to-cart-button w-full py-3 px-6 ${className}`}
+      onClick={() => addToCart(shirt)}
     >
       <span className="cart-icon">🛒</span>
       Add to Cart
